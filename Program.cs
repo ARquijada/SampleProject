45b6dd94f8eb6d2b351945dd941fb1bd6f4611e2
ARquijada <estrellalexis@gmail.com>
2024-06-17 22:05:38 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace AuthenticationUsingArrayOfStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] username = { "username1", "username2", "username3" };
+            string[] password = { "password1", "password2", "password3" };
+
+            Console.Write("Enter Username: ");
+            string usernameInput = Console.ReadLine();
+
+            Console.Write("Enter Password: ");
+            string passwordInput = Console.ReadLine();
+
+            for (int i = 0; i < username.Length; i++)
+            {
+                if (usernameInput.Equals(username[i], StringComparison.InvariantCultureIgnoreCase) && passwordInput.Equals(password[i], StringComparison.InvariantCultureIgnoreCase))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Welcome " + usernameInput);
+                    break;
+                }
+                else if (i >= username.Length - 1)
+                {
+
+                    Console.WriteLine();
+                    Console.WriteLine("Account Not Found!");
+                    break;
+                }
+            }
+        }
+    }
+}
